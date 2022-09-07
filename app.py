f612bb0f9954537b12db8b9cda6b4eac2e82447a
abc-ap <111716104+abc-ap@users.noreply.github.com>
2022-09-07 12:57:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -59,9 +59,5 @@ def testimonials():
 def about():
     return render_template('about.html')
 
-@app.route('/contact/', methods=['GET']) 
-def about():
-    return render_template('contact.html')
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
